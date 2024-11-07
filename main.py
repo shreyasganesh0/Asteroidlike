a718d55a653ed5f11fdbf85bdf6fb132787a4c88
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2024-11-07 14:49:01 -0500

    Create a triangle player class that renders on screen

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import pygame
+from player import Player
+#from shapes import Circle
+
 from constants import *
 def main():
     pygame.init()
+    clock_obj = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
 
@@ -14,7 +20,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock_obj.tick(60)
+        dt /=1000
         
+       
+       
 if __name__ =='__main__':
     main()
